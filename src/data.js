8602302f06a8156fd4e8110879fc5194d75a97c3
Alan Klein <aklein@osisecure.com>
2019-10-18 20:00:42 -0400

    Added new Questions and Answers

--- src/data.js
+++ src/data.js
@@ -4,6 +4,115 @@ var data = [
     questions: [
       {
         points: 100,
+        question: "What tool Creates a React app with no build configuration?",
+        options: [
+          "A. create-react-build",
+          "B. create-react-app",
+          "C. create-react",
+          "D. All of the Above"
+        ],
+        answer: "B. create-react-app"
+      },
+      {
+        points: 200,
+        question: "What type of react component can maintain state?",
+        options: [
+          "A. Functional Component",
+          "B. Logical Component",
+          "C. Stateful Component",
+          "D. Class Component"
+        ],
+        answer: "D. Class Component"
+      },
+      {
+        points: 300,
+        question: "What does JSX stand for?",
+        options: [
+          "A. JavaScript XML",
+          "B. JavaScript eXchange",
+          "C. JavaScipt Shorthand SyntaX",
+          "D. JavaScript SyntaX (10)"
+        ],
+        answer: "A. JavaScript XML"
+      },
+      {
+        points: 400,
+        question: "Which example would be invalid JSX?",
+        options: [
+          "A. <div className=\"Button\" />",
+          "B. <div id=\"Button\" />",
+          "C. <div class=\"Button\" />",
+          "D. All of the Above"
+        ],
+        answer: "C. <div class=\"Button\" />"
+      }
+    ]
+  },
+  {
+    category: "JavaScript",
+    questions: [
+      {
+        points: 100,
+        question: "What are ways of handling Asynchronous Code?",
+        options: [
+          "A. callbacks, returns, promises",
+          "B. async/await, callbacks, returns",
+          "C. callbacks, async/await, promises",
+          "D. returns, try/catch, closures"
+        ],
+        answer: "C. callbacks, async/await, promises"
+      },
+      {
+        points: 200,
+        question: "What are 3 ways to define a variable in JavaScript?",
+        options: [
+          "A. set, var, let",
+          "B. let, const, set",
+          "C. let, value, const",
+          "D. var, const, let"
+        ],
+        answer: "D. var, const, let"
+      },
+      {
+        points: 300,
+        question: "What do three equal signs (===) provide over two equal signs (==)",
+        options: [
+          "A. Optimal Equality",
+          "B. Strict Equality",
+          "C. Not a valid syntax",
+          "D. Used for Comparing Objects"
+        ],
+        answer: "B. Strict Equality"
+      },
+      {
+        points: 400,
+        question: "<What is the case convention used in JavaScript?",
+        options: [
+          "A. snake_case",
+          "B. kebab-case",
+          "C. camelCase",
+          "D. CapitalCase"
+        ],
+        answer: "C. camelCase"
+      }
+    ]
+  },
+  {
+    category: "React 201",
+    questions: [
+      {
+        points: 100,
+        question: "Which company created React?",
+        options: [
+          "A. Google",
+          "B. Facebook",
+          "C. Amazon",
+          "D. Oracle"
+        ],
+        answer: "B. Facebook"
+      },
+      {
+        points: 200,
         question: "What part of your application does React focus on?",
         options: [
           "A. The Model",
@@ -14,184 +123,75 @@ var data = [
         answer: "B. The View"
       },
       {
-        points: 200,
-        question: "<p>What part of your application does React focus on?</p>",
-        options: [
-          "A. The Model",
-          "B. The View",
-          "C. The Controller",
-          "D. All of the Above"
-        ],
-        answer: "B. The View"
-      },
-      {
         points: 300,
-        question: "<p>What part of your application does React focus on?</p>",
+        question: "What is a competing front-end framework to React?",
         options: [
-          "A. The Model",
-          "B. The View",
-          "C. The Controller",
-          "D. All of the Above"
+          "A. JQuery",
+          "B. Express",
+          "C. Angular",
+          "D. Vanilla JavaScript"
         ],
-        answer: "B. The View"
+        answer: "C. Angular"
       },
       {
         points: 400,
-        question: "<p>What part of your application does React focus on?</p>",
+        question: "How is data passed down from one React component to another?",
         options: [
-          "A. The Model",
-          "B. The View",
-          "C. The Controller",
+          "A. props",
+          "B. attributes",
+          "C. classes and id's",
           "D. All of the Above"
         ],
-        answer: "B. The View"
+        answer: "A. props"
       }
     ]
   },
   {
-    category: "React 201",
+    category: "General",
     questions: [
       {
         points: 100,
-        question: "<p>What part of your application does React focus on?</p>",
+        question: "What does DOM stand for?",
         options: [
-          "A. The Model",
-          "B. The View",
-          "C. The Controller",
-          "D. All of the Above"
+          "A. Data Object Model",
+          "B. Document Object Model",
+          "C. Dynamic Object Model",
+          "D. Domain Object Medium"
         ],
-        answer: "B. The View"
+        answer: "B. Document Object Model"
       },
       {
         points: 200,
-        question: "<p>What part of your application does React focus on?</p>",
+        question: "Where does the DOM exist?",
         options: [
-          "A. The Model",
-          "B. The View",
-          "C. The Controller",
-          "D. All of the Above"
+          "A. The Matrix",
+          "B. iOS or Android App",
+          "C. The Browser",
+          "D. The JavaScript Engine"
         ],
-        answer: "B. The View"
+        answer: "C. The Browser"
       },
       {
         points: 300,
-        question: "<p>What part of your application does React focus on?</p>",
+        question: "What is SQL?",
         options: [
-          "A. The Model",
-          "B. The View",
-          "C. The Controller",
-          "D. All of the Above"
+          "A. Structured Query Language",
+          "B. System Query Language",
+          "C. Structured Query Learning",
+          "D. System Query Level"
         ],
-        answer: "B. The View"
+        answer: "A. Structured Query Language"
       },
       {
         points: 400,
-        question: "<p>What part of your application does React focus on?</p>",
+        question: "What feature defines a pure function?",
         options: [
-          "A. The Model",
-          "B. The View",
-          "C. The Controller",
-          "D. All of the Above"
+          "A. No side effects",
+          "B. No return values",
+          "C. Always returns the same result for same argument values",
+          "D. A and C"
         ],
-        answer: "B. The View"
-      }
-    ]
-  },
-  {
-    category: "React 301",
-    questions: [
-      {
-        points: 100,
-        question: "<p>What part of your application does React focus on?</p>",
-        options: [
-          "A. The Model",
-          "B. The View",
-          "C. The Controller",
-          "D. All of the Above"
-        ],
-        answer: "B. The View"
-      },
-      {
-        points: 200,
-        question: "<p>What part of your application does React focus on?</p>",
-        options: [
-          "A. The Model",
-          "B. The View",
-          "C. The Controller",
-          "D. All of the Above"
-        ],
-        answer: "B. The View"
-      },
-      {
-        points: 300,
-        question: "<p>What part of your application does React focus on?</p>",
-        options: [
-          "A. The Model",
-          "B. The View",
-          "C. The Controller",
-          "D. All of the Above"
-        ],
-        answer: "B. The View"
-      },
-      {
-        points: 400,
-        question: "<p>What part of your application does React focus on?</p>",
-        options: [
-          "A. The Model",
-          "B. The View",
-          "C. The Controller",
-          "D. All of the Above"
-        ],
-        answer: "B. The View"
-      }
-    ]
-  },
-  {
-    category: "React 401",
-    questions: [
-      {
-        points: 100,
-        question: "<p>What part of your application does React focus on?</p>",
-        options: [
-          "A. The Model",
-          "B. The View",
-          "C. The Controller",
-          "D. All of the Above"
-        ],
-        answer: "B. The View"
-      },
-      {
-        points: 200,
-        question: "<p>What part of your application does React focus on?</p>",
-        options: [
-          "A. The Model",
-          "B. The View",
-          "C. The Controller",
-          "D. All of the Above"
-        ],
-        answer: "B. The View"
-      },
-      {
-        points: 300,
-        question: "<p>What part of your application does React focus on?</p>",
-        options: [
-          "A. The Model",
-          "B. The View",
-          "C. The Controller",
-          "D. All of the Above"
-        ],
-        answer: "B. The View"
-      },
-      {
-        points: 400,
-        question: "<p>What part of your application does React focus on?</p>",
-        options: [
-          "A. The Model",
-          "B. The View",
-          "C. The Controller",
-          "D. All of the Above"
-        ],
-        answer: "B. The View"
+        answer: "D. A and C"
       }
     ]
   }
